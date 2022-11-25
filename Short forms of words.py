e1c9cadd37291e3b3dba5239b93ff185c18bf0a9
Mohit <115339697+mohitkadiann@users.noreply.github.com>
2022-11-25 13:45:09 +0530

    Update Short forms of words.py

--- Short forms of words.py
+++ Short forms of words.py
@@ -1,6 +1,11 @@
 print("                -:Generating an acronym word from a given sentence:-")
+# Taking a list as input that contains various strings
 alist=eval(input("Enter the list containing multiple strings:"))
 acr=""
+# Splitting the elements of the list that are the strings here
 for i in alist:
+    # Taking the first character of the strings and adding them to make a new string
     acr=acr+i[0]
+# Converting the acronym into upper case
+# And printing the final acronym word
 print(acr.upper())    
